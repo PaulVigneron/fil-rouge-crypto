b78acd39407b6fa65360dbf24db1e81aef82c0fc
gaetanroux <gaetan.roux@ynov.com>
2022-03-02 18:11:44 +0100

    requete api coinmarketcap

---
+++ coinmarketcapAPI.php
@@ -0,0 +1,35 @@
+<?php
+$url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info';
+$parameters = ['symbol' => 'BTC'];
+
+$headers = [
+  'Accepts: application/json',
+  'X-CMC_PRO_API_KEY: 1379c989-f465-4f09-884c-167cfccd8710'
+];
+
+function coin($url, $parameters, $headers) {
+
+
+
+
+  $qs = http_build_query($parameters); // query string encode the parameters
+  $request = "{$url}?{$qs}"; // create the request URL
+
+  print($request);
+
+  $curl = curl_init(); // Get cURL resource
+  // Set cURL options
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $request,            // set the request URL
+    CURLOPT_HTTPHEADER => $headers,     // set the headers 
+    CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+  ));
+
+  $response = curl_exec($curl); // Send the request, save the response
+  print_r(json_decode($response)); // print json decoded response
+  curl_close($curl); // Close request
+
+
+}
+coin($url, $parameters, $headers);
+?>
